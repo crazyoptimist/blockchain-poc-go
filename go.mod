58302837dcd38b61e84e6bc78661ebfdac1a24ec
crazyoptimist <crazyoptimist@mail.com>
2021-10-23 05:40:41 -0500

    :tada:Yay, you now understand what-the-hell is BLOCKCHAIN

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/crazyoptimist/blockchain-poc-go
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+)
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+type BlockChain struct {
+	blocks []*Block
+}
+
+func main() {
+	chain := InitBlockchain()
+
+	chain.AddBlock("first block after genesis")
+	chain.AddBlock("second block after genesis")
+	chain.AddBlock("third block after genesis")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("Previous hash: %x\n", block.PrevHash)
+		fmt.Printf("data: %s\n", block.Data)
+		fmt.Printf("hash: %x\n", block.Hash)
+		fmt.Println("---")
+	}
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	//current last block
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockchain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
